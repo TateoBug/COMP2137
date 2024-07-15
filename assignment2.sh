c42d3dc178b7e357676889e24c05e8620f9d6a3a
Matteo Balechowsky <200557424@student.georgianc.on.ca>
2024-07-15 18:38:53 -0400

    Fixed some bugs, and added UFW rules using if checks

--- assignment2.sh
+++ assignment2.sh
@@ -22,9 +22,10 @@ echo 'Updating the /etc/hosts file: '
 if grep -q "$NewIP $HostName" /etc/hosts; then
     echo 'Server1 IP & Host Name has already been updated'
 else 
-    if grep -q "\b$HostName\b" /etc/hosts; then
+    if grep -q "$HostName" /etc/hosts; then
     	echo "Updating the IP address for $HostName in the /etc/hosts file"
-        sed -i "/\b$HostName\b/c\\$NewIP\t$HostName" /etc/hosts
+    sed -i "s/^.*\s\+$HostName\$/$NewIP\t$HostName/" /etc/hosts
+        echo "Host file was updated!"
     else
         echo "$NewIP $HostName" >> /etc/hosts
         echo "Added IP $NewIP and Host Name $HostName to /etc/hosts"
@@ -45,32 +46,66 @@ for PKG in apache2 squid; do
     else
         echo "Error installing $PKG"
     fi
-fi
+    fi
 done
 
 
 echo '---------------------------------------------'
 echo 'Configuring the Firewall: '
 
-if command -v ufw &> /dev/null; then
+if ! command -v ufw &> /dev/null; then
 echo 'ufw not installed.. Installing ufw...'
-	sudo apt-get update -qq
-	sudo apt-get install ufw -qq
+	apt-get update > /dev/null 2>&1
+	apt-get install ufw > /dev/null 2>&1
 else 
 	echo 'ufw was already installed'
 fi
 
-if ufw status | grep -q "22. *ALLOW.* 172.16.1.200/24"; then
-	echo 'ufw has already been allowed'
+echo 'Enabling UFW...'
+ufw --force enable > /dev/null 2>&1
+if ufw status | grep -q "active"; then
+	echo 'UFW is now active'
 else
-	echo 'Allowing ufw to port 22 on mgmt'
-	ufw allow from 172.16.1.200/24 to any port 22
-	echo 'ufw on mgmt has been configured'
+	echo 'UFW activation failed'
 fi
 
+if ufw status | grep -q "22.*ALLOW.*172.16.1.200/24"; then
+	echo 'ufw has already been allowed on port 22'
+else
+	echo 'Allowing ufw to port 22 on mgmt...'
+	ufw allow from 172.16.1.200/24 to any port 22 > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+	echo 'ufw on mgmt has been configured' 
+else
+	echo 'ufw configuration failed'
+   fi
+fi
+   
+if ufw status | grep -q "{80}/tcp.*ALLOW"; then
+	echo 'UFW rule was already added'
+else
+	echo 'Adding UFW rule...'
+	ufw allow 80/tcp > /dev/null 2>&1
+	if ufw status | grep -q "{80}/tcp.*ALLOW"; then
+	echo 'Rule was successfully added'
+else 
+	echo 'UFW rule failed to update'
+   fi
+fi
+   
+if ufw status | grep -q "{3128}/tcp.*ALLOW"; then
+	echo 'UFW rule was already added'
+else
+	echo 'Adding UFW rule...'
+	ufw allow 3128/tcp > /dev/null 2>&1
+	if ufw status | grep -q "{3128}/tcp.*ALLOW"; then
+	echo 'Rule was successfully added'
+else
+	echo 'UFW rule failed to update'
+   fi
+fi
 echo '---------------------------------------------'
 echo 'Creating User accounts: '
 
 echo '---------------------------------------------'
-echo 'Configuration complete!' 
-
+echo 'Configuration complete!'
